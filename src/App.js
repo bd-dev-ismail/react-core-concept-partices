5bd482293db2bf279d8c1d1d64cc7f80353f51d1
bd-dev-ismail <ismailratul7@gmail.com>
2022-09-22 10:07:18 +0600

    state declare

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+const districtStyle = {
+  backgroundColor: 'yellowgreen',
+  padding: '20px',
+  margin: '20px',
+  borderRadius: '20px'
+}
 function App() {
   return (
     <div className="App">
       <District name="Chittagong" special="Mejban"/>
       <District name="Dhaka" special="Capital"/>
-      <District name="Kulna" special="Ku ku ku"/>
+      <District name="Kulna" special="Ku ku "/>
     </div>
   );
 }
-function District(props){
-  console.log(props);
+function District({name, special}){
+  const [power, setPower] = useState(1);
+  const boostPower = () => setPower(power * 2);
+  const downPower = ()=> setPower(power / 2);
   return (
-    <div>
-      <h3>Name: {props.name}</h3>
-      <p>Specialty: {props.special}</p>
+    <div style={districtStyle}>
+      <h3>Name: {name}</h3>
+      <p>Specialty: {special}</p>
+      <h4>Power: {power}</h4>
+      <button style={{marginRight: '20px'}} onClick={boostPower}>Boost Power</button>
+      <button onClick={downPower}>Down Power</button>
     </div>
   )
 }
