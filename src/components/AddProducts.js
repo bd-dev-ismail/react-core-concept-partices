7b894e4af7d32d41abf15f722ef016e9f926a582
bd-dev-ismail <ismailratul7@gmail.com>
2022-11-01 13:37:31 +0600

    added more functionality

--- src/components/AddProducts.js
+++ src/components/AddProducts.js
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
 
 const AddProducts = () => {
+  const [products, setProducts] = useState({})
+  // console.log(products);
+  const handalAdd = (e) =>{
+    e.preventDefault();
+    fetch("http://localhost:5000/products",{
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(products)
+    }).then(res=> res.json())
+    .then(data => {
+      console.log(data);
+      if(data.success){
+        toast.success(data.message);
+        e.target.reset();
+      }else{
+        toast.error(data.error)
+      }
+    })
+    .catch(err =>{
+      toast.error(err.message)
+    })
+  }
+  const handalBlur = (e)=>{
+    const field = e.target.name;
+    const name = e.target.value;
+    const newProducts = {...products};
+    newProducts[field] = name;
+    setProducts(newProducts);
+  }
     return (
       <div>
         <h3>Please Add a Products</h3>
         <div>
           <div className="hero bg-base-200">
             <div className="hero-content">
-    
               <div className="card  w-full  shadow-2xl bg-base-100">
-                <div className="card-body">
+                <form onSubmit={handalAdd} className="card-body">
                   <div className="form-control">
                     <label className="label">
                       <span className="label-text">Product Name</span>
                     </label>
                     <input
-                    name='product'
+                      onBlur={handalBlur}
+                      required
+                      name="name"
                       type="text"
                       placeholder="Product"
                       className="input input-bordered"
@@ -26,27 +59,31 @@ const AddProducts = () => {
                       <span className="label-text">Product Quantity</span>
                     </label>
                     <input
-                    name='quantity'
+                      onBlur={handalBlur}
+                      required
+                      name="quantity"
                       type="text"
                       placeholder="Quantity"
                       className="input input-bordered"
                     />
-                    </div>
+                  </div>
                   <div className="form-control">
                     <label className="label">
                       <span className="label-text">Product Image URL</span>
                     </label>
                     <input
-                    name='image'
+                      onBlur={handalBlur}
+                      required
+                      name="image"
                       type="text"
                       placeholder="Image URL"
                       className="input input-bordered"
                     />
-                    </div>
+                  </div>
                   <div className="form-control mt-6">
                     <button className="btn btn-primary">Add Product</button>
                   </div>
-                </div>
+                </form>
               </div>
             </div>
           </div>
